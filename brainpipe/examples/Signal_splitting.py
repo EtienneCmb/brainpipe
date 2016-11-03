f274b210d3d8d84c04a3008cfb905eb6e8052a8e
EtienneCmb <e.combrisson@gmail.com>
2016-11-03 11:20:47 +0100

    Add signal spliting example

---
+++ brainpipe/examples/Signal_splitting.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+
+from brainpipe.features import TimeSplit
+
+
+if __name__ == "__main__":
+    # Define a random signal :
+    sf = 256.      # Sampling frequency
+    npts = 30000   # Number of time points
+    t = np.arange(npts) / sf
+    sig = np.sin(2*np.pi*0.10*t)
+
+    # Split signal :
+    sp = TimeSplit(sf, 22, unit='s')
+    xsp = sp.apply(sig, axis=0)
+    tsp = sp.apply(t)[0, :]
+    print(tsp.min(), tsp.max())
+
+    # Plot :
+    plt.figure()
+    plt.subplot(211)
+    plt.plot(t, sig, color='lightgray', label='Original signal')
+    plt.subplot(212)
+    plt.plot(tsp, xsp.T, color='firebrick', label='Splitted signal')
+    plt.show()
