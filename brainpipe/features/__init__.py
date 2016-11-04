51737208d6558de55bff886503bd2f92e0b4669c
EtienneCmb <e.combrisson@gmail.com>
2016-11-04 12:30:43 +0100

    Add Normalization to init

--- brainpipe/features/__init__.py
+++ brainpipe/features/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from .filtering import Chain
-from .utils import (Window, TimeSplit, binarize, binarray, normalize,
+from .utils import (Window, TimeSplit, binarize, binarray, Normalization,
                     time_to_sample, sample_to_time)
--- brainpipe/features/utils/__init__.py
+++ brainpipe/features/utils/__init__.py
@@ -1,4 +1,4 @@
-from .normalize import normalize
+from .normalize import Normalization
 from .window import Window, TimeSplit
 from .makebin import binarize, binarray
 from .unit import time_to_sample, sample_to_time
