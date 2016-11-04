e68446c52063e776e72bf421fc2f44e0cc473731
EtienneCmb <e.combrisson@gmail.com>
2016-11-04 12:30:32 +0100

    Change Normalization example

--- brainpipe/examples/Normalize_array.py
+++ brainpipe/examples/Normalize_array.py
@@ -3,41 +3,57 @@
 """This script illustrate how to normalize an array."""
 import numpy as np
 import matplotlib.pyplot as plt
-from brainpipe.features import normalize
+from brainpipe.features import Normalization
 
 
-def _plot(mat, title=''):
+def _plot(xvec, yvec, mat, title=''):
     """Plotting function."""
-    plt.pcolormesh(mat, cmap='inferno')
+    plt.pcolormesh(xvec, yvec, mat, cmap='inferno')
     plt.colorbar()
-    plt.xlabel('Axis 1'), plt.ylabel('Axis 2')
+    plt.xlabel('Time (s)'), plt.ylabel('Axis 2')
     plt.title(title, y=1.02)
     plt.axis('tight')
 
 
 if __name__ == '__main__':
-    # Define an array A to normalize by B :
-    A = np.random.rand(200, 4000)
-    B = A[:, 20:100]
+    # ---------- Create a random signal ------------
+    sf = 256.0      # Sampling frequency
+    npts = 5000     # Number of points
+    nrep = 50       # An other dimension
+    t = np.arange(npts)/sf
+    yvec = np.arange(nrep)
+    x = np.meshgrid(np.arange(nrep), np.arange(npts))[1].T
+    x = x.astype(float) / x.max()
 
-    # Use the diffrent types of normalization :
-    A0 = normalize(A, B, norm=0, axis=1)
-    t0 = 'No normalization'
-    A1 = normalize(A, B, norm=1, axis=1)
-    t1 = 'A - mean(B)'
-    A2 = normalize(A, B, norm=2, axis=1)
-    t2 = 'A / mean(B)'
-    A3 = normalize(A, B, norm=3, axis=1)
-    t3 = '(A - mean(B))/mean(B)'
-    A4 = normalize(A, B, norm=4, axis=1)
-    t4 = 'Z-score (A - mean(B))/std(B)'
+    # ---------- Normalize -----------
+    # No normalization :
+    no = Normalization(sf)
+    x0 = no.apply(x)
+    # Normalization A-B :
+    no1 = Normalization(sf, norm=1, baseline=(5, 10), unit='s')
+    x1 = no1.apply(x, axis=1)
+    # Normalization A/B :
+    no2 = Normalization(sf, norm='A/B', baseline=(2250, 15257), unit='ms')
+    no2Str = str(no2)
+    x2 = no2.apply(x, axis=1)
+    # Normalization A-B/B :
+    no2.norm = 3 # Update precedent object
+    x3 = no2.apply(x, axis=1)
+    # Normalization z-score :
+    no3 = Normalization(sf, norm=4, baseline=(1000, 1500))
+    x4 = no3.apply(x, axis=1)
 
     # Plot :
-    plt.figure()
-    plt.subplot(151), _plot(A0, t0)
-    plt.subplot(152), _plot(A1, t1)
-    plt.subplot(153), _plot(A2, t2)
-    plt.subplot(154), _plot(A3, t3)
-    plt.subplot(155), _plot(A4, t4)
-
+    plt.subplot(611)
+    _plot(t, yvec, x, title='Original signal')
+    plt.subplot(612)
+    _plot(t, yvec, x0, str(no))
+    plt.subplot(613)
+    _plot(t, yvec, x1, str(no1))
+    plt.subplot(614)
+    _plot(t, yvec, x2, no2Str)
+    plt.subplot(615)
+    _plot(t, yvec, x3, str(no2))
+    plt.subplot(616)
+    _plot(t, yvec, x4, str(no3))
     plt.show()
