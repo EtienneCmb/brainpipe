ec94b7a43e1a5849f71030298ba2a188e4b685ad
EtienneCmb <e.combrisson@gmail.com>
2019-01-30 15:00:20 +0100

    Add hdf5 read / write functions for strings

--- brainpipe/io/__init__.py
+++ brainpipe/io/__init__.py
@@ -1,5 +1,6 @@
 """Brainpipe I/O."""
 from .read_json import load_json, save_json, update_json  # noqa
-from .rw_data import load_file, save_file, safety_save  # noqa
+from .rw_data import (load_file, save_file, safety_save, hdf5_write_str,  # noqa
+                      hdf5_read_str)
 from .read_trc import read_trc  # noqa
 from .intranat import (intranat_db_integrity, intranat_csv)  # noqa
