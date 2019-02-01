23a5e2f24468b4ff6a27a9d9e2fa906f2f66f901
EtienneCmb <e.combrisson@gmail.com>
2019-02-01 08:14:30 +0100

    Add intranat ROI related functions

--- brainpipe/io/__init__.py
+++ brainpipe/io/__init__.py
@@ -3,4 +3,6 @@ from .read_json import load_json, save_json, update_json  # noqa
 from .rw_data import (load_file, save_file, safety_save, hdf5_write_str,  # noqa
                       hdf5_read_str)
 from .read_trc import read_trc  # noqa
-from .intranat import (intranat_db_integrity, intranat_csv)  # noqa
+from .intranat import (intranat_csv, intranat_save_anat, intranat_load_anat,  # noqa
+                       intranat_merge_anatomy, intranat_group_anatomy,
+                       intranat_get_roi)
